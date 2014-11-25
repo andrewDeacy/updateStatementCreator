35dea969853367c05343bd66e54b783bc531efbd
andrewdeacy <adeacydevelopment@gmail.com>
2014-11-25 12:02:05 -0600

    slight update

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 __author__ = 'adeacy'
 import re
 with open("input.txt") as infile, open("output.txt", "w") as outfile:
+    table = input('Enter the name of the table you would like to update')
+    column = input('Enter the name of the column you would like to update') #multiple column update to come in the future
+    value = input('Enter the value you would like to set the column to')
+    outfile.write('Update ' + table)
+    outfile.write('Set ' + column + '= ' + value)
+    outfile.write('Where ')
+    
     for line in infile:
         outfile.write('input = ' + line.lower())
