64a6f131fabe77c59a86a3cdca4eeef790da2f8c
andrewdeacy <adeacydevelopment@gmail.com>
2014-11-25 14:43:00 -0600

    Finished creating a simple query creator

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 __author__ = 'adeacy'
 import re
-with open("input.txt") as infile, open("output.txt", "w") as outfile:
+with open("testData.txt") as infile, open("output.txt", "w") as outfile:
     table = input('Enter the name of the table you would like to update')
     column = input('Enter the name of the column you would like to update') #multiple column update to come in the future
-    value = input('Enter the value you would like to set the column to')
-    outfile.write('Update ' + table)
-    outfile.write('Set ' + column + '= ' + value)
+    value = input('Enter the new value of that column')
+    where = input('Enter the name of column in the datasheet')
+    outfile.write('Update ' + table + '\n')
+    outfile.write('Set ' + column + ' = ' + value + '\n')
     outfile.write('Where ')
-    
+    first = 0
     for line in infile:
-        outfile.write('input = ' + line.lower())
+        if first == 0:
+            outfile.write(where + ' = ' + line.lower())
+            first = 1
+        else:
+            outfile.write('or ' + where + ' = ' + line.lower())
+            first = 1
