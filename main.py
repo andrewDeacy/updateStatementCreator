1d6bf43e7b97e0539e642d2005604f6e9eec4187
andrewdeacy <adeacydevelopment@gmail.com>
2014-11-26 11:41:14 -0600

    update small issue with input

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 __author__ = 'adeacy'
 import re
 with open("testData.txt") as infile, open("output.txt", "w") as outfile:
-    table = input('Enter the name of the table you would like to update')
-    column = input('Enter the name of the column you would like to update') #multiple column update to come in the future
-    value = input('Enter the new value of that column')
-    where = input('Enter the name of column in the datasheet')
+    table = input('Enter the name of the table you would like to update: ')
+    column = input('Enter the name of the column you would like to update: ') #multiple column update to come in the future
+    value = input('Enter the new value of that column: ')
+    where = input('Enter the name of column in the datasheet: ')
     outfile.write('Update ' + table + '\n')
     outfile.write('Set ' + column + ' = ' + value + '\n')
     outfile.write('Where ')
